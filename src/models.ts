9997fb8c6ee765fbf632737347db620311e18fb9
Mario Beltrán Alarcón <belco90@gmail.com>
2020-07-07 22:08:28 +0200

    Add interface for hacker news story

---
+++ src/models.ts
@@ -0,0 +1,11 @@
+export interface HackerNewsStory {
+  id: number;
+  by: string;
+  descendants: number;
+  kids: Array<number>;
+  score: number;
+  time: number;
+  title: string;
+  type: 'story';
+  url: string;
+}
