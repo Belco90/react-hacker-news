d89699eeb646acaa2284d66eea961104a5eec03b
Mario Beltrán Alarcón <belco90@gmail.com>
2020-07-08 21:39:49 +0200

    Include load more mechanism

--- src/components/StoriesTimeline.tsx
+++ src/components/StoriesTimeline.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import { Box, Heading, Spinner, Stack } from '@chakra-ui/core';
+import { Box, Button, Heading, Spinner, Stack, Text } from '@chakra-ui/core';
 import StoryItem from './StoryItem';
 import { readStoriesIndex } from '../api';
+
+const CHUNK_SIZE = 30;
 
 const StoriesTimeline = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const [storiesIds, setStoriesIds] = React.useState<number[]>([]);
+  const [scrollStoriesIds, setScrollStoriesIds] = React.useState<number[]>([]);
+  const [canLoadMore, setCanLoadMore] = React.useState<boolean>(false);
+  const { current: pagination } = React.useRef<{
+    cursor: number;
+  }>({ cursor: 0 });
 
   React.useEffect(() => {
     const retrieveStoriesIds = async () => {
       const data = await readStoriesIndex();
       setStoriesIds(data);
       setIsLoading(false);
+
+      const firstPagination = data.slice(0, CHUNK_SIZE);
+      setScrollStoriesIds(firstPagination);
+      pagination.cursor = firstPagination.length;
+      setCanLoadMore(firstPagination.length < data.length);
     };
 
     retrieveStoriesIds();
   }, []);
+
+  const loadMoreStories = () => {
+    const nextStoriesIdsSlice = storiesIds.slice(
+      pagination.cursor,
+      pagination.cursor + CHUNK_SIZE
+    );
+
+    const newScrollStoriesIds = [...scrollStoriesIds, ...nextStoriesIdsSlice];
+
+    pagination.cursor = newScrollStoriesIds.length;
+    setCanLoadMore(newScrollStoriesIds.length < storiesIds.length);
+
+    setScrollStoriesIds(newScrollStoriesIds);
+  };
 
   if (isLoading) {
     return (
@@ -38,10 +64,22 @@ const StoriesTimeline = () => {
       </Heading>
 
       <Stack spacing={2}>
-        {storiesIds.slice(0, 30).map((storyId) => (
+        {scrollStoriesIds.map((storyId) => (
           <StoryItem key={storyId} id={storyId} />
         ))}
       </Stack>
+
+      <Box mt={5} display="flex" justifyContent="center">
+        {canLoadMore ? (
+          <Button onClick={loadMoreStories} variantColor="brand">
+            Load more
+          </Button>
+        ) : (
+          <Text as="em" mx="auto" color="gray.500">
+            No more news :(
+          </Text>
+        )}
+      </Box>
     </Box>
   );
 };
