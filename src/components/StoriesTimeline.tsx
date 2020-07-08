0a288553211f97c0533c6d721c5b12a20ad9a89b
Mario Beltrán Alarcón <belco90@gmail.com>
2020-07-08 20:37:55 +0200

    Show spinner while new stories are loaded

--- src/components/StoriesTimeline.tsx
+++ src/components/StoriesTimeline.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { Box, Heading, Stack } from '@chakra-ui/core';
+import { Box, Heading, Spinner, Stack } from '@chakra-ui/core';
 import StoryItem from './StoryItem';
 import { readStoriesIndex } from '../api';
 
 const StoriesTimeline = () => {
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
   const [storiesIds, setStoriesIds] = React.useState<number[]>([]);
 
   React.useEffect(() => {
     const retrieveStoriesIds = async () => {
       const data = await readStoriesIndex();
       setStoriesIds(data);
+      setIsLoading(false);
     };
 
     retrieveStoriesIds();
   }, []);
+
+  if (isLoading) {
+    return (
+      <Box
+        minHeight="85vh"
+        width="full"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Spinner color="brand.500" size="xl" thickness="4px" speed="0.65s" />
+      </Box>
+    );
+  }
 
   return (
     <Box my={8}>
