54249af68b46c24f047ab0fda37f64141d8c21ca
Mario Beltrán Alarcón <belco90@gmail.com>
2020-07-09 16:01:48 +0200

    Add cached fetch mechanism

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,57 @@
 const HACKER_NEWS_URI = 'https://hacker-news.firebaseio.com/v0';
 
-export const readStoriesIndex = async () => {
-  const resp = await fetch(`${HACKER_NEWS_URI}/newstories.json`);
+const DEFAULT_FETCH_OPTION: RequestInit = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+  },
+};
 
-  if (resp.ok) {
-    const data = await resp.json();
-    return data;
+const cachedFetch = (url: string) => {
+  const expiry = 5 * 60; // 5 min default
+  const cacheKey = url;
+  const cached = localStorage.getItem(cacheKey);
+  const whenCached = localStorage.getItem(cacheKey + ':ts');
+
+  if (cached !== null && whenCached !== null) {
+    // it was in sessionStorage
+    const age = (Date.now() - Number(whenCached)) / 1000;
+
+    // TODO: check if offline to return cached value too
+    if (age < expiry) {
+      const response = JSON.parse(cached);
+      return Promise.resolve(response);
+    } else {
+      // Clean up this old key
+      localStorage.removeItem(cacheKey);
+      localStorage.removeItem(cacheKey + ':ts');
+    }
   }
 
-  throw new Error('Something went wrong fetching new stories');
+  return fetch(url, DEFAULT_FETCH_OPTION).then((response) => {
+    if (response.ok) {
+      // There is a .json() instead of .text() but
+      // we're going to store it in sessionStorage as
+      // string anyway.
+      // If we don't clone the response, it will be
+      // consumed by the time it's returned. This
+      // way we're being un-intrusive.
+      response
+        .clone()
+        .text()
+        .then((content) => {
+          localStorage.setItem(cacheKey, content);
+          localStorage.setItem(cacheKey + ':ts', String(Date.now()));
+        });
+    }
+    return response.json();
+  });
+};
+
+export const readStoriesIndex = async () => {
+  return cachedFetch(`${HACKER_NEWS_URI}/newstories.json`);
 };
 
 export const readItem = async (id: number) => {
-  const resp = await fetch(`${HACKER_NEWS_URI}/item/${id}.json`);
-
-  if (resp.ok) {
-    const data = await resp.json();
-    return data;
-  }
-
-  throw new Error(`Something went wrong fetching item ${id}`);
+  return cachedFetch(`${HACKER_NEWS_URI}/item/${id}.json`);
 };
