a6fb12e3322d42f7a7299634daad45a0cce9bfc9
Mario Beltrán Alarcón <belco90@gmail.com>
2020-07-12 20:43:45 +0200

    Improve offline visualization

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,12 @@ import { useConnectivityStatus } from '../contexts/connectivity-status';
 
 const Header = () => {
   const connectivityStatus = useConnectivityStatus();
+  const isOnline = connectivityStatus === 'online';
 
   return (
     <Box
       as="header"
-      backgroundColor="brand.400"
+      backgroundColor={isOnline ? 'brand.400' : 'gray.700'}
       mb={{ xs: 2, md: 4 }}
       py={2}
       color="gray.50"
@@ -22,9 +23,11 @@ const Header = () => {
             </span>{' '}
             Hacker News
           </Heading>
-          <Text as="span" fontSize="sm" fontWeight="normal">
-            {connectivityStatus}
-          </Text>
+          {!isOnline && (
+            <Text as="span" fontSize="sm" fontWeight="normal">
+              offline
+            </Text>
+          )}
         </Flex>
       </Container>
     </Box>
