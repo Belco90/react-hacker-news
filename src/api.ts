eda2a98ed46ea896426999200cdbf573db7408a4
Mario Beltrán Alarcón <belco90@gmail.com>
2020-07-12 21:34:49 +0200

    Refactor cached fetch

--- src/api.ts
+++ src/api.ts
@@ -6,46 +6,48 @@ const DEFAULT_FETCH_OPTION: RequestInit = {
     Accept: 'application/json',
   },
 };
-const EXPIRY_SECONDS = 5 * 60; // 5 min default
 
 const cachedFetch = (url: string) => {
   const cacheKey = url;
   const cached = localStorage.getItem(cacheKey);
   const whenCached = localStorage.getItem(cacheKey + ':ts');
-  const isOnline = window.navigator.onLine;
+  const hasCacheMatch = cached !== null && whenCached !== null;
 
-  if (cached !== null && whenCached !== null) {
-    // it was in sessionStorage
-    const age = (Date.now() - Number(whenCached)) / 1000;
+  return fetch(url, DEFAULT_FETCH_OPTION)
+    .then((response) => {
+      if (response.ok) {
+        // If we don't clone the response,
+        // it will be consumed by the time it's returned.
+        response
+          .clone()
+          .text()
+          .then((content) => {
+            // save new cache result
+            localStorage.setItem(cacheKey, content);
+            localStorage.setItem(cacheKey + ':ts', String(Date.now()));
 
-    // Return cached value if offline or not expired
-    if (!isOnline || age < EXPIRY_SECONDS) {
-      const response = JSON.parse(cached);
-      return Promise.resolve(response);
-    } else {
-      // Clean up this old key
-      localStorage.removeItem(cacheKey);
-      localStorage.removeItem(cacheKey + ':ts');
-    }
-  }
+            // if there was previous cache result, delete it
+            if (hasCacheMatch) {
+              localStorage.removeItem(cacheKey);
+              localStorage.removeItem(cacheKey + ':ts');
+            }
+          });
+        return response.json();
+      } else if (hasCacheMatch) {
+        const response = JSON.parse(cached!);
+        return Promise.resolve(response);
+      }
 
-  return fetch(url, DEFAULT_FETCH_OPTION).then((response) => {
-    if (response.ok) {
-      // If we don't clone the response, it will be
-      // consumed by the time it's returned. This
-      // way we're being un-intrusive.
-      response
-        .clone()
-        .text()
-        .then((content) => {
-          localStorage.setItem(cacheKey, content);
-          localStorage.setItem(cacheKey + ':ts', String(Date.now()));
-        });
-      return response.json();
-    }
+      throw new Error(`Impossible to fetch "${url}"`);
+    })
+    .catch((error) => {
+      if (hasCacheMatch) {
+        const response = JSON.parse(cached!);
+        return Promise.resolve(response);
+      }
 
-    throw new Error(`Something went wrong fetching "${url}"`);
-  });
+      throw error;
+    });
 };
 
 export const readStoriesIndex = async () => {
